5d4ca335a81e8e36aa48494ca1a9f8de483c760c
Niek Geerligs <geerligsniek@gmail.com>
2026-06-30 08:54:59 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# neuebrandenbook_catalogue
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/neuebrandenbook_catalogue/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.neuebrandenbook_catalogue
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
